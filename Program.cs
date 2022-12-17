d1b5189ae901f8695a9e33d66cc7bc184b2d6a73
Wiciaki <wiktor.mendalka@outlook.com>
2022-12-17 18:07:46 +0100

    Add project files

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+using Newtonsoft.Json.Linq;
+
+var json = JObject.Parse(File.ReadAllText("transcript.json"));
+
+var sb = new StringBuilder();
+
+foreach (var jobject in json["results"]!)
+{
+    sb.Append(jobject["alternatives"]![0]!["transcript"]);
+    sb.AppendLine();
+}
+
+var result = Regex.Replace(sb.ToString(), "yy+", string.Empty);
+File.WriteAllText("wynik.txt", result);
